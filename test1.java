8ac8b56175a92fc1a1bde389a8f408c79b69c4e7
RamyaShivanna <51778719+RamyaShivanna@users.noreply.github.com>
2019-06-16 13:04:09 +0530

    test1

--- test1.java
+++ test1.java
@@ -4,5 +4,5 @@ public class test1
 public static void main (String[]args)
 {
 System.out.println("hii everyone");
-  System.out.println("good noon and night");
+  System.out.println("good noon and nightand morning");
 }}
